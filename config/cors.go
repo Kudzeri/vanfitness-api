e8b46dd26d76d3b8e3494e82e33f8490788cfb4c
Kudzeri <kudzeri@gmail.com>
2025-02-22 04:44:42 +0500

    ...

--- config/cors.go
+++ config/cors.go
@@ -8,7 +8,7 @@ import (
 
 func SetupCORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
